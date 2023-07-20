4e1606f939f3eb5344ba46aa1456a7efedc649c7
unknown <k200385@nu.edu.pk>
2023-07-20 17:42:37 +0500

    designed login page

--- Audit_Code/resources/views/login-view.blade.php
+++ Audit_Code/resources/views/login-view.blade.php
@@ -4,32 +4,7 @@
 @section('content')
 
 
-
-@if(Session::has('error'))
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <p class="text-bold">{{Session::get('error')}}
-  <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true"></span>
-  </button>
-</div>
-    
-@endif
-
-
-@if(Session::has('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <p class="text-bold">{{Session::get('success')}}
-  <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true"></span>
-  </button>
-</div>
-    
-@endif
-
-
-
-
-<div class="container">
+{{-- <div class="container">
 
 <h1 class="text-center mt-3">Login</h1>
 
@@ -72,8 +47,73 @@
 </div>
 
 
-</div>
+</div> --}}
 
+
+
+<section class="vh-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex align-items-center justify-content-center h-100">
+      <div class="col-md-8 col-lg-7 col-xl-6">
+        <img src="{{asset('/login.jpg')}}"
+          class="img-fluid" alt="Phone image">
+      </div>
+      <div class="col-md-6 col-lg-5 col-xl-5 offset-xl-1 mt-5">
+        <form method="post" action='/login'>
+          @csrf
+          <!-- Email input -->
+          <div class="form-outline mb-3">
+            <input type="email" id="form1Example13" class="form-control form-control-lg" placeholder="Email" name="email" value="{{ old('email') }}" />
+          </div>
+          @if($errors->has('email'))
+          <div class="text-danger">{{ $errors->first('email') }}</div>
+      @endif
+
+          <!-- Password input -->
+          <div class="form-outline mb-1">
+            <input type="password" id="form1Example23" class="form-control form-control-lg" placeholder="Password" name="password" />
+          </div>
+          @if($errors->has('password'))
+          <div class="text-danger">{{ $errors->first('password') }}</div>
+      @endif
+
+          <div class="mb-4">
+    
+            <a href="{{route('password.request')}}">Forgot password?</a>
+          </div>
+
+          <div class="text-center">
+          <!-- Submit button -->
+          <button type="submit" class="btn btn-primary btn-md">Sign in</button>
+        </div>
+
+
+
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@section('scripts')
 
 @if(Session::has('sweetalert'))
 <script>
@@ -85,5 +125,18 @@
     });
 </script> 
 @endif
+
+@if(Session::has('error'))
+<script>
+    swal({
+  title: "{{Session::get('error')}}",
+  icon: "error",
+  closeOnClickOutside: true,
+  timer: 3000,
+    });
+</script> 
+@endif
+
+@endsection
 
 @endsection
--- Audit_Code/resources/views/master.blade.php
+++ Audit_Code/resources/views/master.blade.php
@@ -6,30 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Audit Website</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>  
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>   --}}
   <link rel="stylesheet" href="{{asset('css/style.css')}}">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
 </head>
 <body>
-    <div class="">
     
     @yield('content')
 
-</div>
+
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
    
-   
-    {{-- jquery --}}
-    
-  
-    {{-- 
-     --}}
+
+
+     {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-   {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script> --}}
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>  --}}
  <script>
         $(document).ready(function () {
         $('#sidebarCollapse').on('click', function () {
@@ -39,6 +39,7 @@
 
 </script> 
 
+@yield('scripts')
 
  
 </body>
