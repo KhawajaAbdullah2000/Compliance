862f3c01a1a5da23870af0ac13b40fce49025f09
k200385 <k200385@nu.edu.pk>
2024-07-11 19:03:33 +0500

    fixed excel error

--- Audit_Code/database/seeders/ProjectTypes.php
+++ Audit_Code/database/seeders/ProjectTypes.php
@@ -15,9 +15,9 @@ class ProjectTypes extends Seeder
     public function run(): void
     {
         DB::table('project_types')->insert([
-            ['id' => 1, 'type' => 'PCI-DSS v4-Single-Tenant Service Provider (stSP'],
-            ['id'=>2,'type'=>'PCI-DSS v4-Multi-Tenant Service Provider (mtSP)'],
-            ['id'=>3,'type'=>'PCI-DSS v4-Merchant'],
+            ['id' => 1, 'type' => 'PCI-DSS v4-Single-Tenant Service Provider (stSP)'],
+            // ['id'=>2,'type'=>'PCI-DSS v4-Multi-Tenant Service Provider (mtSP)'],
+            // ['id'=>3,'type'=>'PCI-DSS v4-Merchant'],
             ['id'=>4,'type'=>'ISO 27001:2022']
 
         ]);
